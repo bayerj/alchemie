e1d83db270671c6d8ddd85755420624f5cff3953
Maximilian Soelch <maximilian.soelch@gmx.de>
2015-07-23 16:44:28 +0200

    expanded python stack depth for pickle | indendation fix

--- scripts/alc.py
+++ scripts/alc.py
@@ -35,19 +35,23 @@ if platform.system() == 'Windows':
     ctypes.CDLL(os.path.join(basepath, 'core', 'libmmd.dll'))
     ctypes.CDLL(os.path.join(basepath, 'core', 'libifcoremd.dll'))
 
-# Now set our handler for CTRL_C_EVENT. Other control event
-# types will chain to the next handler.
-def handler(dwCtrlType, hook_sigint=thread.interrupt_main):
-    if dwCtrlType == 0: # CTRL_C_EVENT
-        hook_sigint()
-        return 1 # don't chain to the next handler
-    return 0 # chain to the next handler
+    # Now set our handler for CTRL_C_EVENT. Other control event
+    # types will chain to the next handler.
+    def handler(dwCtrlType, hook_sigint=thread.interrupt_main):
+        if dwCtrlType == 0: # CTRL_C_EVENT
+            hook_sigint()
+            return 1 # don't chain to the next handler
+        return 0 # chain to the next handler
 
-win32api.SetConsoleCtrlHandler(handler, 1)
+    win32api.SetConsoleCtrlHandler(handler, 1)
 
 from alchemie import contrib
 from breze.learn.utils import JsonForgivingEncoder
 # from breze.learn.base import UnsupervisedBrezeWrapperBase
+
+rl = sys.getrecursionlimit()
+sys.setrecursionlimit(int(2*rl))
+
 
 
 def load_module(m):
