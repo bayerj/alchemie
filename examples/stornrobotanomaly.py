86006020088a0939b390288e5503efaf7c2d2d06
Maximilian Soelch <maximilian.soelch@gmx.de>
2015-09-29 15:57:42 +0200

    PEP8 and small readability improvements

--- examples/stornrobotanomaly.py
+++ examples/stornrobotanomaly.py
@@ -9,36 +9,34 @@ Options:
     --amount=<n>        Amount of configurations to create. [default: 1]
     --no-json-log       Do not use JSON for logging.
 """
-from alchemie import alc
-import docopt
 
+import docopt
 
 import os
 import sys
 import h5py
-import signal
-import time
+from shutil import copyfile
+
+import numpy as np
+from sklearn.grid_search import ParameterSampler
+import matplotlib.pyplot as plt
 
 from theano.printing import pydotprint, pp, debugprint
+from theano.configparser import config_files_from_theanorc as theanorc_path
+from theano.compile.nanguardmode import NanGuardMode
+from theano.compile import MonitorMode
+
+from alchemie import alc
+from alchemie.contrib import git_log
+from alchemie.trainer.trainer import Trainer
+from alchemie.trainer.report import OneLinePrinter
 
 from breze.learn.base import cast_array_to_local_type
 from breze.learn.sgvb import stornrobotanomaly_storns as storns
 from breze.learn.data import interleave, padzeros, split
-from alchemie.trainer.trainer import Trainer
-from alchemie.trainer.report import OneLinePrinter
 
 import climin.initialize
 from climin.stops import Any
-import numpy as np
-
-from sklearn.grid_search import ParameterSampler
-
-
-from alchemie.contrib import git_log
-
-from theano.configparser import config_files_from_theanorc as theanorc_path
-from shutil import copyfile
-
 
 
 class StornRobotAnomaly(object):
@@ -46,11 +44,11 @@ class StornRobotAnomaly(object):
         """Return a string preamble for the the resulting cfg.py file"""
         return ''
 
-
     def draw_pars(self, n=1):
         """
         :param n: number of random configurations
-        :return: sklearn ParameterSampler object producing n random samples from the specified grid
+        :return: sklearn ParameterSampler object producing n random samples
+        from the specified grid
         """
         # class OptimizerDistribution(object):
         #     def rvs(self):
@@ -65,100 +63,103 @@ class StornRobotAnomaly(object):
         #         return 'rmsprop', sample
         class RandomSeed(object):
             def rvs(self):
-                return int(np.random.uniform(low=-2**31, high=2**31-1))
+                return int(np.random.uniform(low=-2 ** 31, high=2 ** 31 - 1))
+
         grid = {
             # stochastic gradient algorithm
             'optimizer': ['adadelta'],
             # batch size for SG determination
-            'batch_size': [5,10,20],
+            'batch_size': [5, 10, 20],
 
             # number of generative and recognition layers, respectively
-            'n_gen_layers': [1,2,3,4],
-            'n_recog_layers': [1,2,3,4],
-            # number of hidden units per generative or recognition layer, respectively
-            'n_gen_hidden_units': [2**i for i in range(1,6)],
-            'n_rec_hidden_units': [2**i for i in range(1,6)],
+            'n_gen_layers': [1, 2, 3, 4],
+            'n_recog_layers': [1, 2, 3, 4],
+            # number of hidden units per generative or recognition layer
+            'n_gen_hidden_units': [2 ** i for i in range(1, 6)],
+            'n_rec_hidden_units': [2 ** i for i in range(1, 6)],
 
-            # type of activation/transfer function in generative or recognition part, respectively
+            # type of transfer function in generative or recognition part
             'transfer_gen': ['sigmoid', 'tanh', 'rectifier', 'lstm'],
             'transfer_rec': ['sigmoid', 'tanh', 'rectifier', 'lstm'],
 
             # latent dimensionality of VAE
-            'n_latents': [2**i for i in range(1,6)],
+            'n_latents': [2 ** i for i in range(1, 6)],
             # assumption on distribution of latent units
             'latent_assumption': ['DiagGauss'],
 
-            # standard deviation of noise on output (necessary to enforce compression)
-            'out_std': [10**-i for i in range(5)],
+            # standard deviation of noise on output
+            'out_std': [10 ** -i for i in range(5)],
 
             # standard deviation of random weight initilization
-            'par_init_std': [2**i  for i in range(-6,2)],
+            'par_init_std': [2 ** i for i in range(-6, 2)],
             'par_init_std_in': [0.000001],
 
             # dropout rate in the input layer or hidden layer(s), respectively
-            'p_dropout_inpt': [i/10. for i in range(1,11)],
-            'p_dropout_hiddens': [i/10. for i in range(1,11)],
-            'p_dropout_hidden_to_out': [i/10. for i in range(1,11)],
-            'p_dropout_shortcut': [i/10. for i in range(1,11)],
+            'p_dropout_inpt': [i / 10. for i in range(1, 11)],
+            'p_dropout_hiddens': [i / 10. for i in range(1, 11)],
+            'p_dropout_hidden_to_out': [i / 10. for i in range(1, 11)],
+            'p_dropout_shortcut': [i / 10. for i in range(1, 11)],
 
             # maximum training time in minutes
             'minutes': [300],
 
             'seed': RandomSeed()
-
         }
 
         sampler = ParameterSampler(grid, n)
         return sampler
 
-
     def load_data(self, pars):
 
-        datafile = '//brml.tum.de/dfs/public/Datasets/BaxterCollision/data/bluebear/bluebear.h5'
+        datafile =  '//brml.tum.de/dfs/public/Datasets/BaxterCollision/data' \
+                    '/bluebear/bluebear.h5'
         with h5py.File(datafile) as fp:
-            dev_seqs = [np.array(fp['dev'][i]) for  i in fp['dev']]
-            txs = [np.array(fp['test'][i]) for  i in fp['test']]
+            dev_seqs = [np.array(fp['dev'][i]) for i in fp['dev']]
+            txs = [np.array(fp['test'][i]) for i in fp['test']]
             tzs = np.array(fp['test_anomaly_label'])
 
         x = dev_seqs[:80]
         m = np.concatenate(x).mean(axis=0)
         s = np.concatenate(x).std(axis=0)
 
-        x = [(xx-m)/s for xx in x]
-        vx = [(vxx-m)/s for vxx in dev_seqs[80:]]
-        txs = [(tx-m)/s for tx in txs]
+        x = [(xx - m) / s for xx in x]
+        vx = [(vxx - m) / s for vxx in dev_seqs[80:]]
+        txs = [(tx - m) / s for tx in txs]
 
-        X, M = tuple(interleave(l) for l in padzeros(x, front=False, return_mask=True))
-        VX, VM = tuple(interleave(l) for l in padzeros(vx, front=False, return_mask=True))
-        TX, TM = tuple(interleave(l) for l in padzeros(txs, front=False, return_mask=True))
-        M, VM, TM = M[:,:,:1], VM[:,:,:1], TM[:,:,:1]
+        X, M = tuple(
+            interleave(l) for l in padzeros(x, front=False, return_mask=True))
+        VX, VM = tuple(
+            interleave(l) for l in padzeros(vx, front=False, return_mask=True))
+        TX, TM = tuple(
+            interleave(l) for l in padzeros(txs, front=False, return_mask=True))
+        M, VM, TM = M[:, :, :1], VM[:, :, :1], TM[:, :, :1]
 
-        X, M, VX, VM, TX, TM = [cast_array_to_local_type(i) for i in (X, M, VX, VM, TX, TM)]
+        X, M, VX, VM, TX, TM = [cast_array_to_local_type(i) for i in
+                                (X, M, VX, VM, TX, TM)]
 
         return {
-            'train': (X,M),
-            'val': (VX,VM),
-            'test': (TX,TM)
+            'train': (X, M),
+            'val': (VX, VM),
+            'test': (TX, TM)
         }
-
 
     def new_trainer(self, pars, data):
         #########
         # LOGGING GIT COMMITS
         #########
-        modules =  ['theano', 'breze', 'climin', 'alchemie']
+        modules = ['theano', 'breze', 'climin', 'alchemie']
         cwd = os.getcwd()
         try:
             gl = git_log(modules)
         except:
             gl = 'gitlog failed. Probably no git installed.'
-        with open(os.path.join(cwd, 'gitlog.txt'),'w') as result:
+        with open(os.path.join(cwd, 'gitlog.txt'), 'w') as result:
             result.write(gl)
 
-
         # This should maybe be moved to contrib as a static method;
-        # note the different file name format - this is just for documentation purposes
-        copyfile(theanorc_path(),os.path.join(cwd,'theanorc.txt'))
+        # note the different file name format - this is just for
+        # documentation purposes
+        copyfile(theanorc_path(), os.path.join(cwd, 'theanorc.txt'))
 
         #########
         # BUILDING AND INITIALIZING MODEL FROM pars
@@ -175,13 +176,14 @@ class StornRobotAnomaly(object):
 
         m = mystorn(
             n_inpt=int(data['test'][0].shape[2]),
-            n_hiddens_recog=[pars['n_rec_hidden_units']] * pars['n_recog_layers'],
+            n_hiddens_recog=[pars['n_rec_hidden_units']] * pars[
+                'n_recog_layers'],
             n_latent=pars['n_latents'],
             n_hiddens_gen=[pars['n_gen_hidden_units']] * pars['n_gen_layers'],
             recog_transfers=[pars['transfer_rec']] * pars['n_recog_layers'],
             gen_transfers=[pars['transfer_gen']] * pars['n_gen_layers'],
             use_imp_weight=True,
-            #assumptions=Assumptions(),
+            # assumptions=Assumptions(),
             batch_size=pars['batch_size'],
             optimizer=optimizer,
             p_dropout_inpt=pars['p_dropout_inpt'],
@@ -190,17 +192,20 @@ class StornRobotAnomaly(object):
             p_dropout_shortcut=pars['p_dropout_shortcut']
         )
 
-        #m.shared_std = True
-        #m.fixed_std = theano.shared(np.ones((1,)) * .1, broadcastable=(True,))
+        m.mode = NanGuardMode(nan_is_error=True, inf_is_error=False,
+                              big_is_error=False)
+
+        # m.shared_std = True
+        # m.fixed_std = theano.shared(np.ones((1,)) * .1, broadcastable=(True,))
 
         m.initialize(
             par_std=pars['par_init_std'],
             par_std_in=pars['par_init_std_in'],
-            #sparsify_affine=sparsify_in,
-            #sparsify_rec=sparsify_rec,
+            # sparsify_affine=sparsify_in,
+            # sparsify_rec=sparsify_rec,
             spectral_radius=None)
 
-        #m.parameters[m.vae.gen.std][...] = 0.01
+        # m.parameters[m.vae.gen.std][...] = 0.01
         # print 'here'
         # print 'here'
         # print 'here'
@@ -217,28 +222,33 @@ class StornRobotAnomaly(object):
         t = Trainer(
             model=m,
             data=data,
-            stop=climin.stops.Any([
-                                    climin.stops.TimeElapsed(pars['minutes'] * 60),
-                                    climin.stops.IsNaN(keys=['train_loss', 'val_loss'])
-                                    ]),
+            stop=climin.stops.Any(
+                [climin.stops.TimeElapsed(pars['minutes'] * 60),
+                 climin.stops.IsNaN(keys=['train_loss', 'val_loss'])
+                 ]),
             pause=climin.stops.ModuloNIterations(n_report),
-            report=OneLinePrinter(['n_iter', 'runtime', 'train_loss', 'val_loss'],spaces=[6,'10.2f','15.8f','15.8f']),
-            interrupt=Any([climin.stops.OnSignal()]),#, climin.stops.OnSignal(sig=signal.SIGBREAK)]),
-        )#loss_keys=['train', 'val'])
+            report=OneLinePrinter(
+                keys=['n_iter', 'runtime', 'train_loss', 'val_loss'],
+                spaces=[6, '10.2f', '15.8f', '15.8f']),
+            interrupt=Any([climin.stops.OnSignal()]),
+            # climin.stops.OnSignal(sig=signal.SIGBREAK)]),
+            # loss_keys=['train', 'val'])
+        )
 
         return t
-
 
     def make_report(self, pars, trainer, data):
         last_pars = trainer.switch_pars(trainer.best_pars)
 
         # fig, ax = plt.subplots(1, 1, figsize=(16, 9))
         # ax.plot([i['loss'] for i in trainer.infos[0:]])
-        # fig.savefig('loss.pdf', transparent=True, frameon=False, bbox_inches='tight', pad_inches=.05)
+        # fig.savefig('loss.pdf', transparent=True, frameon=False,
+        #             bbox_inches='tight', pad_inches=.05)
         #
         # fig, ax = plt.subplots(1, 1, figsize=(16, 9))
         # ax.plot([i['val_loss'] for i in trainer.infos[0:]])
-        # fig.savefig('val_loss.pdf', transparent=True, frameon=False, bbox_inches='tight', pad_inches=.05)
+        # fig.savefig('val_loss.pdf', transparent=True, frameon=False,
+        #             bbox_inches='tight', pad_inches=.05)
         #
         # ###################
         # # RECONSTRUCTION
@@ -255,33 +265,36 @@ class StornRobotAnomaly(object):
         # ax[1].plot(Y[:, 0], Y[:, 2], 'rx')
         # ax[2].plot(X[:, 1], X[:, 2], 'bx')
         # ax[2].plot(Y[:, 1], Y[:, 2], 'rx')
-        # fig.savefig('reconstruction.pdf', transparent=True, frameon=False, bbox_inches='tight', pad_inches=.05)
+        # fig.savefig('reconstruction.pdf', transparent=True, frameon=False,
+        #             bbox_inches='tight', pad_inches=.05)
         #
         # fig, ax3d = plt.subplots(1,1, figsize=(15,11))
         # ax3d = fig.add_subplot(111, projection='3d')
         # ax3d.plot(X[:,0], X[:, 1], X[:, 2], 'bx')
-        # fig.savefig('reconstruction3d.pdf', transparent=True, frameon=False, bbox_inches='tight', pad_inches=.05)
+        # fig.savefig('reconstruction3d.pdf', transparent=True, frameon=False,
+        #             bbox_inches='tight', pad_inches=.05)
         #
         # ###################
         # # SAMPLING
         # ###################
         # f_generate = trainer.model.function(['sample'], 'output')
-        #
+        # shape = (X.shape[0], pars['n_latents'])
         # if pars['latent_assumption'] == 'KW':
-        #     qs = np.random.uniform(size=X.shape[0]*pars['n_latents']).reshape((X.shape[0], pars['n_latents'])).astype('float32')
-        # else: # includes case 'DiagGauss'
-        #     qs = np.random.standard_normal((X.shape[0], pars['n_latents'])).astype('float32')
+        #     qs = np.random.uniform(size=shape).astype('float32')
+        # else:  # includes case 'DiagGauss'
+        #     qs = np.random.standard_normal(size=shape).astype('float32')
         #
         # S = f_generate(qs)
         #
-        # alpha=0.1
+        # alpha = 0.1
         # fig, ax = plt.subplots(4, 1, figsize=(15, 36))
         # ax[0].plot(S[:, 0], S[:, 1], 'ro', alpha=alpha)
         # ax[1].plot(S[:, 0], S[:, 2], 'ro', alpha=alpha)
         # ax[2].plot(S[:, 1], S[:, 2], 'ro', alpha=alpha)
         # ax3d = fig.add_subplot(4, 1, 4, projection='3d')
         # ax3d.plot(S[:, 0], S[:, 1], S[:,2], 'ro', alpha=alpha)
-        # fig.savefig('sampling.pdf', transparent=True, frameon=False, bbox_inches='tight', pad_inches=.05)
+        # fig.savefig('sampling.pdf', transparent=True, frameon=False,
+        #             bbox_inches='tight', pad_inches=.05)
 
         result = {'train_loss': trainer.score(*data['train']),
                   'val_loss': trainer.score(*data['val']),
